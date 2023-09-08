dfa2fcda2986a8d4e7a91a5d5e3765efc268beab
chizzyuchenna <uchennachizua>
2023-09-08 12:03:14 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
- * main - Entry point
+ * main - Prints the alphabetic
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	int n;
+	char c;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
-	else
-		printf("%d is negative\n", n);
-
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
 	return (0);
+
 }
+
